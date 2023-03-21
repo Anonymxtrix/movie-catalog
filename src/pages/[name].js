498774bfcbf167e6b0f9ca09c1e4ecb792521d21
Bryan Loh <lamofacade@gmail.com>
2023-03-21 18:33:38 +0800

    Complete base for movie details page

--- src/pages/[name].js
+++ src/pages/[name].js
@@ -1,3 +1,31 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+import data from "@/fixtures/data.js";
+
 export default function MovieDetails() {
-  return <main>Movie details page</main>;
+  const router = useRouter();
+  const name = router.query.name;
+  const movie = data.find((datum) => datum.name == name);
+
+  if (!movie) return <main>Movie not found.</main>;
+
+  const parsedSynopsis = movie.synopsis.replaceAll("<br />", "\n");
+
+  return (
+    <main className="p-6">
+      <Link href={`..`}>
+        <div className="mb-3">
+          <button className="px-4 py-2 rounded border border-gray-200 shadow hover:hover:bg-gray-100">
+            back
+          </button>
+        </div>
+      </Link>
+      <div className="mb-3">
+        <h1 className="font-bold text-2xl">{movie.name}</h1>
+        <p>{movie.genre}</p>
+        <p className="text-gray-400">{movie.productionYear}</p>
+      </div>
+      <p className="whitespace-pre-wrap">{parsedSynopsis}</p>
+    </main>
+  );
 }
