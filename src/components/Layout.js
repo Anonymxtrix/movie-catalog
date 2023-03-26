be33d88855402072ffb03c899ae6725072052645
Bryan Loh <lamofacade@gmail.com>
2023-03-26 13:08:17 +0800

    Add hover effects to home button

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,14 +5,18 @@ const Layout = ({ children }) => {
   return (
     <>
       <nav className="flex-no-wrap relative flex w-full items-center py-4">
-        <Link className="flex items-center cursor-pointer" href={`/`}>
+        <Link
+          className="flex px-2 items-center cursor-pointer hover:bg-gray-100 rounded-lg"
+          href={`/`}
+        >
           <Image
+            className="-translate-x-2"
             src="/icons8-circled-play-96.png"
             alt="Website Logo"
             width={50}
             height={50}
           />
-          <h1 className="ml-1 text-3xl">Movies</h1>
+          <h1 className="text-3xl">Movies</h1>
         </Link>
       </nav>
       <div>{children}</div>
