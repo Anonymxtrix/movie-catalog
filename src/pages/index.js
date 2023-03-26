c50a351e3dd1de148adfdaa3e68224c546b0712c
Bryan Loh <lamofacade@gmail.com>
2023-03-26 13:45:00 +0800

    Add no movies found UI to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { useContext, useState } from "react";
 import { MoviesData } from "@/context/movies.js";
 import Page from "@/components/Page.js";
@@ -26,6 +27,7 @@ export default function Home() {
       return selectedYears.has(movie.productionYear);
     })
     .sort((a, b) => b.productionYear - a.productionYear);
+  const hasMovies = filteredMoves.length > 0;
 
   const handleGenreFilterChange = (selectedGenres) => {
     setSelectedGenres(new Set(selectedGenres));
@@ -50,26 +52,41 @@ export default function Home() {
           onChange={handleYearFilterChange}
         />
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {filteredMoves.map((movie, index) => {
-          return (
-            <Link
-              className="cursor-pointer"
-              key={index}
-              href={`/${movie.name}`}
-            >
-              <div className="h-full p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-100">
-                <div className="mb-3">
-                  <h1 className="font-bold text-xl">{movie.name}</h1>
-                  <h2>{movie.genre}</h2>
-                  <h3 className="text-gray-400">{movie.productionYear}</h3>
+      {!hasMovies && (
+        <div className="mt-40 flex flex-col items-center justify-center">
+          <Image
+            src="/undraw_movie_night_re_9umk.svg"
+            alt="No Movies"
+            width={800}
+            height={315}
+          />
+          <h1 className="mt-6 text-bold text-2xl text-gray-400">
+            Oops, no movie was found...
+          </h1>
+        </div>
+      )}
+      {hasMovies && (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {filteredMoves.map((movie, index) => {
+            return (
+              <Link
+                className="cursor-pointer"
+                key={index}
+                href={`/${movie.name}`}
+              >
+                <div className="h-full p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                  <div className="mb-3">
+                    <h1 className="font-bold text-xl">{movie.name}</h1>
+                    <h2>{movie.genre}</h2>
+                    <h3 className="text-gray-400">{movie.productionYear}</h3>
+                  </div>
+                  <p className="">{movie.synopsisShort}</p>
                 </div>
-                <p className="">{movie.synopsisShort}</p>
-              </div>
-            </Link>
-          );
-        })}
-      </div>
+              </Link>
+            );
+          })}
+        </div>
+      )}
     </Page>
   );
 }
