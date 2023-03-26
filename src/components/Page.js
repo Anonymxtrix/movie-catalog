c9afafb4d21cd6c1ca87e29ba5ec06af6bc70755
Bryan Loh <lamofacade@gmail.com>
2023-03-26 11:23:04 +0800

    Add padding to bottom of page

--- src/components/Page.js
+++ src/components/Page.js
@@ -15,7 +15,7 @@ const Page = ({ title, children }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/icons8-circled-play-96.png" />
       </Head>
-      <main className="container mx-auto px-6">
+      <main className="container mx-auto px-6 pb-6">
         <Layout>{children}</Layout>
       </main>
     </>
